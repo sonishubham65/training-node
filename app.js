ef755f73d942ebf08cd0387f5dde8d0624712940
subhamsoni <sonishubham65@gmail.com>
2020-08-01 10:40:39 +0530

    Added environment variables and initilize mongoose.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,12 +24,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +38,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+/**
+ * Database connection with mongoose
+ */
+const mongoose = require('mongoose');
+mongoose.connect(`${process.env.DBURI}/${process.env.DBNAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is started.");
+})
 
 module.exports = app;
