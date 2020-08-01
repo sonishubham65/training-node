843b43c6a7fc0c603cf38cca537fd9da79559abb
subhamsoni <sonishubham65@gmail.com>
2020-08-01 13:41:55 +0530

    added request validation with JOI.

--- controller/user.js
+++ controller/user.js
@@ -1,18 +1,82 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const Joi = require('@hapi/joi');
+const signupSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(30)
+        .label("Name")
+        .required()
+        .pattern(new RegExp(/^[A-Z a-z 0-9.]+$/))
+        .error(errors => {
+            errors.forEach(error => {
+                console.log(error);
+                switch (error.code) {
+                    case 'string.pattern.base': {
+                        error.message = '"Name" should contain letters and number only';
+                    } break;
+                }
+            })
+            return errors;
+        }),
+    password: Joi.string()
+        .min(8)
+        .max(30)
+        .label("Password")
+        .required()
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,30}$'))
+        .error(errors => {
+            errors.forEach(error => {
+                console.log(error);
+                switch (error.code) {
+                    case 'string.pattern.base': {
+                        error.message = '"Password" should contain at least a capital letter, a small letter, a number and a special charcter';
+                    } break;
+                }
+            })
+            return errors;
+        }),
+
+    email: Joi.string()
+        .required()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'in'] } })
+        .label("Email"),
+    role: Joi.any().allow('employee', 'manager').label("Role").required()
+});
+
 module.exports.signup = async (req) => {
-    var user = await User.findOne({ email: req.body.email });
-    if (user) {
-        return false;
+    // validation
+    var { error, value } = await signupSchema.validate(req.body);
+    if (error) {
+        // return with error message
+        return {
+            statusCode: 422,
+            message: error.message
+        }
     } else {
-        let salt = await bcrypt.genSalt(parseInt(process.env.ENCRYPTION_saltRounds));
-        let hash = await bcrypt.hash(req.body.password, salt);
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hash,
-            role: req.body.role,
-        });
-        return true;
+        var user = await User.findOne({ email: value.email });
+        if (user) {
+            // return with duplicate resource
+            return {
+                statusCode: 409,
+                message: "User already exists with this email address."
+            }
+        } else {
+            // Create a hash for password
+            let salt = await bcrypt.genSalt(parseInt(process.env.ENCRYPTION_saltRounds));
+            let hash = await bcrypt.hash(value.password, salt);
+            user = await User.create({
+                name: value.name,
+                email: value.email,
+                password: hash,
+                role: value.role,
+            });
+            // return successful
+            return {
+                statusCode: 201,
+                message: "You're registered successfully."
+            }
+        }
     }
+
 }
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,7 @@ const user = require('../controller/user');
 router.post('/signup', async (req, res, next) => {
   try {
     let signupResponse = await user.signup(req);
-    if (signupResponse) {
-      res.status(200).json({ message: "You're registered successfully." })
-    } else {
-      res.status(201).json({ messag: "User already exists with this email address." });
-    }
+    res.status(signupResponse.statusCode).json({ message: signupResponse.message })
   } catch (e) {
     res.status(500).json({
       messag: e.message
