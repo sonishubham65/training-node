a46b6bee5e8f1752d61c568bcdb35c00309b7710
subhamsoni <sonishubham65@gmail.com>
2020-08-06 10:51:49 +0530

    changed the path of files

--- routes/employee/index.js
+++ routes/employee/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const authentication = require('../../middleware/authentication');
 const authorization = require('../../middleware/authorization');
 
-var resume = require('./resume')
+var resume = require('./resume');
+var position = require('./position')
 router.use('/resume', authentication, authorization.employee, resume);
+router.use('/position', authentication, authorization.employee, position);
 module.exports = router;
