8fac941f73dfbc38ae6b03f97bc3b9e95b2e0fb8
subhamsoni <sonishubham65@gmail.com>
2020-08-07 09:36:08 +0530

    test case added for resume update again

--- test-cases/3.Employee/resume.test.js
+++ test-cases/3.Employee/resume.test.js
@@ -5,11 +5,11 @@ let server = require('../../app');
 const path = require('path')
 var expect = chai.expect;
 const fs = require('fs');
-let { ManagerToken, EmployeeToken } = require('../config');
 let correctFile = fs.readFileSync(path.join(__dirname, '../../test-files/Shubham Nagarro Resume.docx'));
 let wrongFile = fs.readFileSync(path.join(__dirname, '../../test-files/shubham-soni-jaipur-profile-pic.jpg'));
 
 describe("Resume update API", () => {
+    let config = require('../config');
     context('Resume update:', () => {
         it("1. Authentication", (done) => {
             chai.request(server)
@@ -26,7 +26,7 @@ describe("Resume update API", () => {
         it("2. Authorization", (done) => {
             chai.request(server)
                 .put("/employee/resume")
-                .set({ "Authorization": `Bearer ${ManagerToken}` })
+                .set({ "Authorization": `Bearer ${config.ManagerToken}` })
                 .then((response) => {
                     console.log(response.body)
                     expect(response.statusCode).to.equal(403);
@@ -41,7 +41,7 @@ describe("Resume update API", () => {
         it("3. Validation of type", (done) => {
             chai.request(server)
                 .put("/employee/resume")
-                .set({ "Authorization": `Bearer ${EmployeeToken}` })
+                .set({ "Authorization": `Bearer ${config.EmployeeToken}` })
                 .attach('resume', wrongFile, 'shubham-soni-jaipur-profile-pic.jpg')
                 .type('form').then((response) => {
                     expect(response.statusCode).to.equal(422);
@@ -58,7 +58,22 @@ describe("Resume update API", () => {
                 .put("/employee/resume")
                 //.field('a', "b")
                 .set('Content-Type', 'multipart/form-data')
-                .set({ "Authorization": `Bearer ${EmployeeToken}` })
+                .set({ "Authorization": `Bearer ${config.EmployeeToken}` })
+                .attach('resume', correctFile, 'Shubham Nagarro Resume.docx').then((response) => {
+                    expect(response.statusCode).to.equal(200);
+                    expect(response.body).to.be.an('object').that.has.property('message');
+                    expect(response.body.message).to.be.a("string");
+                    done();
+                }).catch(err => {
+                    done(err)
+                })
+        })
+        it("5. Update Again", (done) => {
+            chai.request(server)
+                .put("/employee/resume")
+                //.field('a', "b")
+                .set('Content-Type', 'multipart/form-data')
+                .set({ "Authorization": `Bearer ${config.EmployeeToken}` })
                 .attach('resume', correctFile, 'Shubham Nagarro Resume.docx').then((response) => {
                     expect(response.statusCode).to.equal(200);
                     expect(response.body).to.be.an('object').that.has.property('message');
