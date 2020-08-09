7c30e7ff265f5f919710e2f8c37c8bc5cd84ae0b
subhamsoni <sonishubham65@gmail.com>
2020-08-09 16:57:55 +0530

    test cases updated for login api

--- test-cases/1.Account/user.test.js
+++ test-cases/1.Account/user.test.js
@@ -139,6 +139,8 @@ describe("Testing account API", () => {
                 }).then((response) => {
                     expect(response.statusCode).to.equal(200);
                     expect(response.body).to.contain.all.keys('data', 'token');
+                    expect(response.body.token).to.be.an('object');
+                    expect(response.body.token).to.contain.all.keys('access', 'refresh');
                     expect(response.body.data).to.contain.all.keys('role', '_id', 'name', 'email', 'created_at', 'login_at');
                     expect(response.body.data.role).to.equal('employee');
                     config.EmployeeToken = response.body.token;
@@ -156,6 +158,8 @@ describe("Testing account API", () => {
                 }).then((response) => {
                     expect(response.statusCode).to.equal(200);
                     expect(response.body).to.contain.all.keys('data', 'token');
+                    expect(response.body.token).to.be.an('object');
+                    expect(response.body.token).to.contain.all.keys('access', 'refresh');
                     expect(response.body.data).to.contain.all.keys('role', '_id', 'name', 'email', 'created_at', 'login_at');
                     expect(response.body.data.role).to.equal('manager');
                     config.ManagerToken = response.body.token;
